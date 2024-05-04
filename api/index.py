8a079b5ede697971312d7174686a5d49e9288ec1
dinozoiddev <38674879+keeandev@users.noreply.github.com>
2024-05-04 16:36:20 -0500

    trial: form-data parsing

--- api/index.py
+++ api/index.py
@@ -2,18 +2,18 @@ from http.server import BaseHTTPRequestHandler
 
 import json
 import cgi
+from io import BytesIO
 from pdfminer.high_level import extract_text
 from pdfminer.layout import LAParams
 
 
 class handler(BaseHTTPRequestHandler):
 
-    def extract_pdf_content(pdf_file):
-        # Extract text from PDF
+    def extract_pdf_content(self, pdf_content):
         total_pages = 0
         page_contents = []
 
-        with open(pdf_file, 'rb') as f:
+        with BytesIO(pdf_content) as f:
             for page_num, page in enumerate(extract_text(f, laparams=LAParams(), output_type='dict')):
                 total_pages += 1
                 page_content = page['text']
@@ -29,32 +29,25 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write('Welcome to the PDFMiner API!\n'.encode('utf-8'))
         return
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
 
-        if 'Content-Type' in self.headers:
-            content_type = self.headers['Content-Type']
-            if content_type.startswith('multipart/form-data'):
-                form = {}
-                for item in body.decode('utf-8').split('\r\n'):
-                    if item.startswith('Content-Disposition'):
-                        _, params = item.split('; ')
-                        name = params.split('=')[1].strip('"')
-                        form[name] = body.split(item.encode())[
-                            1].strip().strip(b'--\r\n')
+def do_POST(self):
+    content_type, params = cgi.parse_header(self.headers['content-type'])
 
-        if 'file' in form:
-            pdf_content = form['file']
-            try:
+    if content_type == 'multipart/form-data':
+        boundary = params['boundary'].encode()
+
+        try:
+            form_data = cgi.parse_multipart(self.rfile, {'boundary': boundary})
+            if b'file' in form_data:
+                pdf_content = form_data[b'file'][0]
                 result = self.extract_pdf_content(pdf_content)
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json.dumps(result).encode())
-            except Exception as e:
-                self.send_error(
-                    500, 'Error extracting PDF content: {}'.format(e))
-            return
-        else:
-            self.send_error(400, 'Missing \'file\' in FormData.')
+            else:
+                self.send_error(400, 'Missing \'file\' in FormData.')
+        except Exception as e:
+            self.send_error(500, 'Error extracting PDF content: {}'.format(e))
+    else:
+        self.send_error(400, 'Bad Request: Unsupported Content-Type')
